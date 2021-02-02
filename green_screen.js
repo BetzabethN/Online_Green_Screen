8c04c1e98770b57332dfb957d1899925fab070b6
Betzabeth Narvaez <betzynarv@gmail.com>
2021-02-02 14:06:02 -0600

    fix canvas and image variables

--- green_screen.js
+++ green_screen.js
@@ -1,12 +1,15 @@
+var image = null;
+var imgcanvas;
+
 function upload() {
     //get canvas
-    var imgcanvas = document.getElementById("can");
+    imgcanvas = document.getElementById("can");
 
     //get file input
     var fileinput = document.getElementById("finput");
 
     //make new simple image of fileinput
-    var image = new SimpleImage (fileinput);
+    image = new SimpleImage(fileinput);
 
     //draw simple image on canvas
     image.drawTo(imgcanvas);
