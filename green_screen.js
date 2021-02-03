0572d0c4dd1b63182aec1727371d061c3c28b9fe
Betzabeth Narvaez <betzynarv@gmail.com>
2021-02-03 12:58:38 -0600

    modify to take two images and do green screen

--- green_screen.js
+++ green_screen.js
@@ -1,17 +1,54 @@
-var image = null;
-var imgcanvas;
+var fgImage = null;
+var bgImage = null;
+var can1;
+var can2;
 
-function upload() {
-    //get canvas
-    imgcanvas = document.getElementById("can");
+//show foreground image in canvas 1
+function loadForegroundImage() {
+    var imgFile = document.getElementById("fgfile");
+    fgImage = new SimpleImage(imgFile)
+    can1 = document.getElementById("can-F");
+    fgImage.drawTo(can1)
+}
 
-    //get file input
-    var fileinput = document.getElementById("finput");
+//show background image in canvas 2
+function loadBackgroundImage() {
+    var imgFile2 = document.getElementById("bgfile");
+    bgImage = new SimpleImage(imgFile2);
+    can2 = document.getElementById("can-B");
+    bgImage.drawTo(can2);
 
-    //make new simple image of fileinput
-    image = new SimpleImage(fileinput);
+}
 
-    //draw simple image on canvas
-    image.drawTo(imgcanvas);
-    
+function greenScreen() {
+    //check if foreground image is loaded or completely finished loaded
+    if (fgImage == null || !fgImage.complete()) {
+        alert("Foreground not loaded!");
+    }
+    //check if background image is loaded or completely finished loaded
+    if (bgImage == null || !bgImage.complete()) {
+        alert("Background not loaded!");
+    }
+    clearCanvas();
+
+    // creat new image that will be composite
+    var output = new SimpleImage(fgImage.getWidth(), fgImage.getHeight());
+
+    //go through every pixel of the foreground image
+    for (var pixel of fgImage.values()) {
+        var x = pixel.getX();
+        var y = pixel.getY();
+        //check if pixel is green in order to replace with background
+        if (pixel.getGreen() > 240) {
+            var bgPixel = bgImage.getPixel(x, y);
+            output.setPixel(x, y, bgPixel);
+        }
+        //if it is not green, it stays the same
+        else {
+            output.setPixel(x, y, pixel);
+        }
+    }
+
+    //show composite on canvas 1
+    output.drawTo(can1);
 }
